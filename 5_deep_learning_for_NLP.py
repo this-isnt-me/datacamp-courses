51be0955ef05604839b2beb0fc644b1393fb2d5f
this-isnt-me <32744330+this-isnt-me@users.noreply.github.com>
2021-02-01 20:34:54 +0000

    Update 5_deep_learning_for_NLP.py

--- 5_deep_learning_for_NLP.py
+++ 5_deep_learning_for_NLP.py
@@ -28,3 +28,33 @@ model.add(Dense(1, init='uniform', activation='sigmoid'))
 # Compile the model and get gradients
 model.compile(loss='binary_crossentropy', optimizer='adam')
 gradients = backend.gradients(model.output, model.trainable_weights)
+
+
+# Vanishing Gradients
+
+#Before you start working on more robust applications of language generation, it's best to learn how to identify when you are suffering from the vanishing gradient problem. 
+#In this exercise, you will train the network you created in the last exercise using random training data.
+
+#To run a model in Tensorflow, you need to initialize a tensorflow session first by using the InteractiveSession() function. 
+#Then you will initialize all variables using the global_variables_initializer() function.
+
+#You will execute gradients node which you created in the last exercise inside this tensorflow session. 
+#You will also check some of the gradient values from different layers. 
+#Note that tensorflow has been imported as tf. 
+#To know how to run a basic model in Tensorflow, check out this post.
+
+# Create a dummy input vector
+input_vector = np.random.random((1,8))
+
+# Create a tensorflow session to run the network
+sess = tf.InteractiveSession()
+
+# Initialize all the variables
+sess.run(tf.global_variables_initializer())
+
+# Evaluate the gradients using the training examples
+evaluated_gradients = sess.run(gradients,feed_dict={model.input:input_vector})
+
+# Print gradient values from third layer and two nodes of the second layer
+print(evaluated_gradients[4])
+print(evaluated_gradients[2][4])
